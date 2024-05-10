dfe020da2ded089ce9b4101350bc8599e176bd0e
giojano1 <giorgi.janelidze64@gmail.com>
2024-05-10 17:08:16 +0400

    wo

--- js/main.js
+++ js/main.js
@@ -40,23 +40,39 @@ createBtn.forEach(function (create) {
       newList.textContent = trimmedValue;
       cardsBox.appendChild(newList);
 
-      const moveBtn = document.createElement("div");
-      moveBtn.className = "edit";
-      newList.appendChild(moveBtn);
+      const cardMan = document.createElement("div");
+      cardMan.className = "edit";
+      newList.appendChild(cardMan);
 
-      const moveBtnimg = document.createElement("img");
+      const moveBtn = document.createElement("img");
       const removeBtn = document.createElement("img");
-      moveBtnimg.src = "img/arrow.svg";
+      moveBtn.src = "img/arrow.svg";
       removeBtn.src = "img/cancel.svg";
 
-      moveBtn.appendChild(moveBtnimg);
-      moveBtn.appendChild(removeBtn);
-
-      removeBtn.addEventListener("click", function () {
-        cardsBox.removeChild(newList);
-      });
+      cardMan.appendChild(moveBtn);
+      cardMan.appendChild(removeBtn);
 
       textContent.value = "";
+
+      // Remove card
+      removeBtn.addEventListener("click", function () {
+        const card = this.closest(".card");
+        const cardsBox = card.closest(".cardsBox");
+        cardsBox.removeChild(card);
+      });
+      //move card
+      moveBtn.addEventListener("click", function () {
+        const sourceBox = this.closest(".container");
+        const destinationBox = sourceBox.nextElementSibling; // Assuming the destination is always the next sibling container
+
+        const card = this.closest(".card");
+
+        // Remove card from source container
+        sourceBox.querySelector(".cardsBox").removeChild(card);
+
+        // Append card to destination container
+        destinationBox.querySelector(".cardsBox").appendChild(card);
+      });
     } else {
       const createCardDiv = this.closest(".box").querySelector(".createCard");
       const addCardDiv = this.closest(".box").querySelector(".addCard");
@@ -64,7 +80,7 @@ createBtn.forEach(function (create) {
 
       addCardInfo.style.display = "none";
       createCardDiv.style.display = "none";
-      addCardDiv.style.display = "flex";
+      addCardDiv.style.display = xw"flex";
     }
   });
 });
